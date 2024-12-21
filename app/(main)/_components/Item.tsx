29f11a5f44c3b32d22e2cfce3b496225acda372c
unknown <fordariasek@gmail.com>
2024-12-21 14:07:15 +0200

    Add archivation logic

--- app/(main)/_components/Item.tsx
+++ app/(main)/_components/Item.tsx
@@ -1,9 +1,11 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Skeleton } from '@/components/ui/skeleton'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { cn } from '@/lib/utils'
+import { useUser } from '@clerk/clerk-react'
 import { useMutation } from 'convex/react'
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from 'lucide-react'
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, Plus, Trash } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React, { MouseEvent } from 'react'
 import { toast } from 'sonner'
@@ -35,9 +37,12 @@ const Item = ({
     icon: Icon
 }: ItemProps) => {
     const create = useMutation(api.documents.create)
-    const router = useRouter()
+    const archive = useMutation(api.documents.archivate)
 
-    const ChevronIcon = expanded ? ChevronDown: ChevronRight
+    const router = useRouter()
+    const user = useUser()
+
+    const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
     const handleExpand = (event: MouseEvent) => {
         event.stopPropagation()
@@ -66,56 +71,99 @@ const Item = ({
         })
     }
 
+    const onArchive = (e: MouseEvent) => {
+        e.stopPropagation()
+        if (!id) return
+
+        const pr = archive({ id })
+
+        toast.promise(pr, {
+            loading: 'Moving to trash...',
+            success: 'Note moved to trash',
+            error: 'Failed to archive the note'
+        })
+    }
+
     return (
         <div
-        onClick={onClick}
-        role='button'
-        className={cn(
-            'group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium',
-            active && 'bg-primary/5 text-primary'
-        )}
-        style={{ paddingLeft: level ? `${level * 12 + 12}px` : '12px' }}
-    >
-        {!!id && (
-            <div 
-                role='button'
-                className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
-                onClick={handleExpand}
-            >
-                <ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground' />
-            </div>
-        )}
-        {
-            documentIcon
-                ? (<div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>)
-                : <Icon className='shrink-0 h-[18px] mr-2 text-muted-foreground ' />
-        }
-        <span className='truncate'>
-            {label}
-        </span>
-        {
-            isSearch && (
-                <kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground'>
-                    <span className='text-xs'>CNTR</span>K
-                </kbd>
-            )
-        }
-        {!!id && (
-            <div className='ml-auto flex items-center gap-x-2'>
+            onClick={onClick}
+            role='button'
+            className={cn(
+                'group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium',
+                active && 'bg-primary/5 text-primary'
+            )}
+            style={{ paddingLeft: level ? `${level * 12 + 12}px` : '12px' }}
+        >
+            {!!id && (
                 <div
-                    className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600'
                     role='button'
-                    onClick={onCreate}
+                    className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
+                    onClick={handleExpand}
                 >
-                    <Plus className='h-4 w-4 text-muted-foreground' />
+                    <ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground' />
                 </div>
-            </div>
-        )}
-    </div>
-  )
+            )}
+            {
+                documentIcon
+                    ? (<div className='shrink-0 mr-2 text-[18px]'>{documentIcon}</div>)
+                    : <Icon className='shrink-0 h-[18px] mr-2 text-muted-foreground ' />
+            }
+            <span className='truncate'>
+                {label}
+            </span>
+            {
+                isSearch && (
+                    <kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground'>
+                        <span className='text-xs'>CNTR</span>K
+                    </kbd>
+                )
+            }
+            {!!id && (
+                <div className='ml-auto flex items-center gap-x-2'>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger
+                            asChild
+                            onClick={(e) => e.stopPropagation()}
+                        >
+                            <div
+                                role='button'
+                                className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'
+                            >
+                                <MoreHorizontal className='h-4 w-4 text-muted-foreground' />
+                            </div>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            className='w-60'
+                            forceMount
+                            align='start'
+                            side='right'
+                        >
+                            <DropdownMenuItem
+                                onClick={onArchive}
+                            >
+                                <Trash className='h-4 w-4 mr-2' />
+                                Delete
+                            </DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <div className='text-xs text-muted-foreground p-2'>
+                                Last edited by {user.user?.fullName}
+                            </div>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                    <div
+                        className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:bg-neutral-600'
+                        role='button'
+                        onClick={onCreate}
+                    >
+                        <Plus className='h-4 w-4 text-muted-foreground' />
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
 
-Item.Skeleton = function ItemSceleton({level}: {level?: number}) {
+Item.Skeleton = function ItemSceleton({ level }: { level?: number }) {
 
     return (<div
         style={{
--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,44 @@ import { v } from 'convex/values'
 import { mutation, query } from './_generated/server'
 import { Doc, Id } from './_generated/dataModel'
 
+export const archivate = mutation({
+    args: {
+        id: v.id('documents')
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity()
+
+        if (!identity) {
+            throw new Error('Not authenticated')
+        }
+
+        const userId = identity.subject
+
+        const doc = await ctx.db.get(args.id)
+
+        if (!doc) throw new Error('Document is not found')
+        if (doc.userId !== userId) throw new Error('Unauthorized')
+
+        const archivateChildren = async (documentId: Id<'documents'>) => {
+            const children = await ctx.db.query('documents')
+                .withIndex('by_user_parent', q => q.eq('userId', userId).eq('parentDocument', documentId))
+                .collect()
+
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchived: true
+                })
+
+                await archivateChildren(child._id)
+            }
+        }
+
+        const result = await ctx.db.patch(args.id, { isArchived: true})
+        archivateChildren(args.id)
+
+        return result
+    }
+})
 
 export const getSidebar = query({
     args: {
